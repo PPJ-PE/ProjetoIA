229d2b087dd63f4bf3de544d3fbe518b6184bc83
NakaVostro <ra00297288@pucsp.edu.br>
2023-06-27 13:49:25 -0300

    Start

--- Assets/#Game/Scripts/Gameplay/Guns/BaseBullet.cs
+++ Assets/#Game/Scripts/Gameplay/Guns/BaseBullet.cs
@@ -4,15 +4,34 @@ using UnityEngine;
 
 public class BaseBullet : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    [Header("Cache")]
+
+    private BaseGun _parentGun;
+    private Rigidbody _rb;
+    private Collider _collider;
+    private MeshRenderer _meshRenderer;
+
+    protected virtual void Start() {
+        _rb = GetComponent<Rigidbody>();
+        _meshRenderer = GetComponent<MeshRenderer>();
+        _collider = GetComponent<Collider>();
+    }
+
+    protected virtual void OnTriggerEnter(Collider other) {
+        //if(other.GetComponent<>())
+    }
+
+    public virtual void Activate(bool active) {
+        _rb.isKinematic = !active;
+        _collider.enabled = active;
+        _meshRenderer.enabled = active;
+        if (active) Shoot();
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected virtual void Shoot() {
+        transform.position = _parentGun.ShotPoint.position;
+        transform.rotation = _parentGun.ShotPoint.rotation;
+        _rb.velocity = transform.forward * _parentGun.BulletSpeed;
     }
 }
--- Assets/#Game/Scripts/Gameplay/Guns/BaseGun.cs
+++ Assets/#Game/Scripts/Gameplay/Guns/BaseGun.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class BaseGun : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    [Header("Ammo")]
+
+    [SerializeField] protected BaseBullet _bulletPrefab; // Use GO + cache BaseBullet?
+    [SerializeField] protected int _ammoMax;
+    private int _ammoCurrent;
+
+    [Header("Aim")]
+
+    [SerializeField] protected Transform _shotPoint;
+    public Transform ShotPoint { get { return _shotPoint; } }
+    [SerializeField] protected float _shotCooldown;
+    [SerializeField] protected float _bulletSpeed;
+    public float BulletSpeed { get { return _bulletSpeed; } }
+    [Tooltip("The radius of a circle at 1m distance from the fire point, wich inside bullets randomly go to (higher chance to get in the center)")]
+    [SerializeField] protected float _shotSpread;
+    [SerializeField] protected float _reloadDuration; // Instead be "Base" duration ?
+
+    [Header("Cache")]
+
+    private BaseBullet[] _bulletCache;
+
+    protected virtual void Start() {
+        _bulletCache = new BaseBullet[_ammoMax];
+        for (int i = 0; i < _ammoMax; i++) _bulletCache[i] = Instantiate(_bulletPrefab); // Prefab should come deactivated
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    protected virtual void Shoot() {
+        if (_ammoCurrent > 0) {
+            _ammoCurrent--;
+
+        }
     }
+
 }
